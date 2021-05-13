db8e69dc613c00c08d38c218326f764403bdcdad
Robin <robin.kok.91@gmail.com>
2021-05-13 21:03:45 +0200

    EDITS TO wordlist.txt
    - Edited wordlist.txt to make sure words are working and correct.

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Hangman {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("wordlist.txt"));
+
+        while(scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
 
 
 
+    }
 }
